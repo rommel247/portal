313a06da873abe8b53a5ea0f89234ac033cd958f
rommel <rommel.pineda.git@gmail.com>
2021-05-02 00:58:31 +0300

    added EF package

---
+++ artar.com.sa/src/portal.api/Data/ArtContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using portal.api.Data.Entities;
+namespace portal.api.Data
+{
+    public class ArtContext : DbContext
+    {
+        public ArtContext(DbContextOptions<ArtContext> options) : base(options)
+        {
+        }
+        //TODO: here are the  DBSet<entities>
+    }
+}
---
+++ artar.com.sa/src/portal.api/Data/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portal.api.Data.Entities
+{
+    //https://geeksarray.com/blog/entity-framework-core-database-first-tutorial
+    public partial class Department
+    {
+        public Department()
+        {
+
+        }
+
+    }
+}
